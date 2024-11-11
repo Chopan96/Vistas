63bbaad9b42b6c536afb7dc68f3ddc9662d67637
JavINACAP <javier.quinteros04@inacapmail.cl>
2024-11-10 22:14:26 -0300

    historial_paciente

--- Paciente/loginpaciente.js
+++ Paciente/loginpaciente.js
@@ -7,7 +7,7 @@ function validarLoginP() {
     // Validar las credenciales
       if (username === 'paciente@gmail.com' && password === '1234') {
       // Redirige a la página principal si las credenciales son correctas
-      window.location.href = 'historialmP.html';
+      window.location.href = 'historialP.html';
       } else {
       // Muestra el mensaje de error si las credenciales son incorrectas
       errorMessage.textContent = 'Correo o contraseña incorrectos. Por favor, intenta de nuevo.';
